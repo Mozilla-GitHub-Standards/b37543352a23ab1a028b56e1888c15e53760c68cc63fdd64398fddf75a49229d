ffb0f069f604144dbd156ca8663e795ddf200579
Brian Hourigan <bhourigan@gmail.com>
2015-07-04 22:59:52 -0400

    <Hack>

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,4 +12,22 @@ class dns {
             $package_prefix="bind"
         }
     }
+
+    /* <Hack> */
+    file {
+        "/var/named/.ssh/id_rsa":
+            ensure  => present,
+            mode    => '0600',
+            owner   => "named-update",
+            group   => "named-update",
+    }
+
+    file {
+        "/var/named/.ssh/id_rsa.pub":
+            ensure  => present,
+            mode    => '0600',
+            owner   => "named-update",
+            group   => "named-update",
+    }
+    /* </Hack> */
 }
