f25222fb6d7c02a0404c397bba80814a3ae026c1
Brian Hourigan <bhourigan@gmail.com>
2015-09-13 22:11:17 -0400

    cpanm fixes

--- manifests/datadog.pp
+++ manifests/datadog.pp
@@ -1,14 +1,13 @@
 class dns::datadog {
     package {
-        ['perl-App-cpanminus', 'perl-LWP-Protocol-https']:
+        ['perl-App-cpanminus', 'perl-LWP-Protocol-https', 'perl-File-Tail']:
             ensure => installed,
-    }
+    } ->
 
     exec {
         'install-dogstatsd-perl':
             path => '/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/aws/bin:/root/bin',
             command => 'cpanm https://github.com/binary-com/dogstatsd-perl/archive/0.04.tar.gz',
             unless => 'test -f /usr/local/share/perl5/DataDog/DogStatsd.pm',
-            require => Package['perl-App-cpanminus', 'perl-LWP-Protocol-https'],
     }
 }
